ba0bca3d8b8d36fd33dba574fd14755978c1d70f
AlinaSau <saushkina_lina@mail.ru>
2023-06-02 21:35:55 +0300

    modified

--- src/const.js
+++ src/const.js
@@ -1,11 +1,11 @@
 export const TRIP_EVENT_TYPES = [
-    'taxi',
-    'bus',
-    'train',
-    'ship',
-    'drive',
-    'flight',
-    'check-in',
-    'sightseeing',
-    'restaurant',
-  ];
+  'taxi',
+  'bus',
+  'train',
+  'ship',
+  'drive',
+  'flight',
+  'check-in',
+  'sightseeing',
+  'restaurant',
+];
