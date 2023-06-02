91162b64114a54ef807ea4dd6ed1ed680d60c274
AlinaSau <saushkina_lina@mail.ru>
2023-06-02 21:07:07 +0300

    modified

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -2,23 +2,22 @@ import { render } from '../render';
 import TripEventsListView from '../view/trip-events-list.js';
 import EventEditorView from '../view/event-editor-view.js';
 import TripEventView from '../view/trip-event-view.js';
-import EventListSortingView from '../view/event-list-sorting-view.js'
+import EventListSortingView from '../view/event-list-sorting-view.js';
 
 class TripPresenter {
-    tripEventsList = new TripEventsListView();
+  tripEventsList = new TripEventsListView();
   
-    init(container) {
-      this.container = container;
+  init(container) {
+    this.container = container;
   
-      render(new EventListSortingView(), this.container);
-      render(this.tripEventsList, this.container);
-      this.tripEventsList.addEvent(new EventEditorView());
+    render(new EventListSortingView(), this.container);
+    render(this.tripEventsList, this.container);
+    this.tripEventsList.addEvent(new EventEditorView());
   
-      for (let i = 0; i < 3; i++) {
-        this.tripEventsList.addEvent(new TripEventView());
-      }
+    for (let i = 0; i < 3; i++) {
+      this.tripEventsList.addEvent(new TripEventView());
     }
   }
-  
+}
+
   export default TripPresenter;
-  
