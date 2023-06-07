3d6eb0fd997c30df438e4e1def51abc7aae02ab3
AlinaSau <saushkina_lina@mail.ru>
2023-06-07 15:52:12 +0300

    module8-task1

---
+++ src/view/trip-points-list-view.js
@@ -0,0 +1,11 @@
+import AbstractView from '../framework/view/abstract-view.js';
+
+const createTripPointsListTemplate = () => `
+  <ul class="trip-events__list"></ul>
+`;
+
+export default class TripPointsListView extends AbstractView {
+  get template() {
+    return createTripPointsListTemplate();
+  }
+}
